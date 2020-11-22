629ed6dc27311298e307746df75d7955425654bb
Michael Richard <michael.richard.ing@gmail.com>
2020-11-22 07:48:16 -0500

    Add code to deploy on Kubernetes
    Signed-off-by: Michael Richard <michael.richard.ing@gmail.com>

---
+++ current_versions.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# This file gets used from generate-envrc.sh but it is also used standalone by
+# automation that needs to get the version of the programs currently supported
+# in sandbox
+
+export OSIE_DOWNLOAD_LINK=https://tinkerbell-oss.s3.amazonaws.com/osie-uploads/osie-v0-n=366,c=1aec189,b=master.tar.gz
+export TINKERBELL_TINK_WORKER_IMAGE=quay.io/tinkerbell/tink-worker:sha-0e8e5733
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/tinkerbell/sandbox
+
+go 1.15
+
+require (
+	github.com/gianarb/vagrant-go v0.0.0-20200902133321-62ba563fe383
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/tinkerbell/tink v0.0.0-20201119184533-a2d22a0ece3d
+	golang.org/x/sys v0.0.0-20200909081042-eff7692f9009 // indirect
+)
